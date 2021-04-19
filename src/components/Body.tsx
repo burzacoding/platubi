40c034ef313607efa284fe5f37e173aa14decc36
Cesar Pintos <pintos.cesar01@gmail.com>
2021-04-18 21:41:15 -0300

    Inicialización del proyecto
    -Implementación del .editorconfig
    -Eliminación de archivos innecesarios del create-react-app
    -Se agrega una carpeta con assets (necesarios para landing page)
    -Se agregan dependencias:
    Redux, Thunk, React Router, Firebase, Axios, ChartsJS.

---
+++ src/components/Body.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export interface NavProps {
+  
+}
+ 
+const Nav: React.SFC<NavProps> = () => {
+  return (
+    <div className="body-class"></div>
+  );
+}
+ 
+export default Nav;
---
+++ src/utils/utils.ts
@@ -0,0 +1,2 @@
+let hola = "holaaa"
+export {}
