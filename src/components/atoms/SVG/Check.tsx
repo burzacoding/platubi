cc8d30aece1fc224334535fba02e97ad4b407384
Cesar Pintos <pintos.cesar01@gmail.com>
2021-06-08 23:59:09 -0300

    Cambios menores al componente check

--- src/components/atoms/SVG/Check.tsx
+++ src/components/atoms/SVG/Check.tsx
@@ -4,7 +4,7 @@ import { ThemeColorPicker } from '../../../Utils/Utils';
 const CheckSvg = styled.svg`
   position: absolute;
   left: 0px;
-  stop-color: ${p => ThemeColorPicker(p, '#096635', '#1269B4')};
+  stroke: ${p => ThemeColorPicker(p, '#096635', '#1269B4')};
   cursor: pointer;
 `
 
@@ -17,13 +17,7 @@ export interface CheckProps {
 const Check: React.SFC<CheckProps> = () => {
   return (
   <CheckSvg width="28" height="22" viewBox="0 0 28 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2 11.6333L11.7143 19L26 2" stroke="url(#paint0_linear-555107)" stroke-width="4"/>
-    <defs>
-    <linearGradient id="paint0_linear-555107" x1="1.13043" y1="10" x2="26.8696" y2="10" gradientUnits="userSpaceOnUse">
-    <stop stopColor="#1B73BE"/>
-    <stop offset="1" stopColor="#0DCA4F"/>
-    </linearGradient>
-    </defs>
+    <path d="M2 11.6333L11.7143 19L26 2" stroke-width="4"/>
   </CheckSvg>
   );
 }
