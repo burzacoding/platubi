fac34c02f8e76e89a3cb892b87d59389c45c6c29
Cesar Pintos <pintos.cesar01@gmail.com>
2021-07-09 23:48:14 -0300

    useMainCalc re arranged using useMemo

--- src/Hooks/dashboardLogic/useMainCalc.ts
+++ src/Hooks/dashboardLogic/useMainCalc.ts
@@ -1,106 +1,123 @@
 import { useApi } from "../../Contexts/ApiContext";
 import { useDashboard } from "../../Contexts/DashboardContext";
+import { fixDecimals } from "../../Utils/Utils";
+import { useMemo, useState } from 'react'
 
+interface detailedArrayProps {
+  value: number,
+  valueUSD: number,
+  symbol: string,
+  percentage: number,
+  name: string,
+  price: number,
+}
 
 export const useMainCalc = () => {
+
+  const [totalWorth, setTotalWorth] = useState(0)
+  const [detailedArray, setDetailedArray] = useState<detailedArrayProps[]>([] as detailedArrayProps[])
   
   const { userData } = useDashboard()
+  
   const { cryptoPrices, currenciesPrices, currenciesList } = useApi()
 
   const getObjectFromCryptoID = (id: number) => cryptoPrices.data.filter(el => el.id === id)[0]
   const getSymbolFromCryptoId = (id: number) => getObjectFromCryptoID(id).symbol
   const getObjectFromCryptoSymbol = (symbol: string) => cryptoPrices.data.filter(el => el.symbol === symbol)[0]
-
   
+  const symbolsArray: string[] = []
+  const namesArray: string[] = []
+  const valuesArray: number[] = []
+  const pricesArray: number[] = []
   const worthsArray: number[] = []
+  
 
-  if (userData && userData.registers) {
-    const registers = userData.registers.filter(el => el.visible === true)
-    const symbolsArray: string[] = []
-    const namesArray: string[] = []
-    const valuesArray: number[] = []
-    const pricesArray: number[] = []
+  useMemo(() => {
     
-    if (registers?.length !== 0) {
-      for (let register of registers!) {
-        // console.log(register.symbol);
-        if (register.isCrypto) {
-          if (symbolsArray.indexOf(getSymbolFromCryptoId(parseInt(register.symbol))) === -1) {
-            symbolsArray.push(getSymbolFromCryptoId(parseInt(register.symbol)))
-          }
-        } else {
-          if (symbolsArray.indexOf(register.symbol) === -1) {
-            symbolsArray.push(register.symbol)
-          }
-        }
-        // console.log('Vuelta: ', register.symbol, 'symbolsArray: ', symbolsArray);
-      }
-      // console.log('symbolsArray: ', symbolsArray);
+    if (userData && userData.registers) {
+      const registers = userData.registers.filter(el => el.visible === true)
       
-      for (let symbolIndx = 0; symbolIndx < symbolsArray.length; symbolIndx++) {
-        for (let register of registers) {
-          if (!register.isCrypto) {
-            if (symbolsArray[symbolIndx] === register.symbol) {
-              valuesArray[symbolIndx] = valuesArray[symbolIndx] ? valuesArray[symbolIndx] +  register.value : 0 + register.value
+      if (registers?.length !== 0) {
+        for (let register of registers!) {
+          // console.log(register.symbol);
+          if (register.isCrypto) {
+            if (symbolsArray.indexOf(getSymbolFromCryptoId(parseInt(register.symbol))) === -1) {
+              symbolsArray.push(getSymbolFromCryptoId(parseInt(register.symbol)))
             }
           } else {
-            if (symbolsArray[symbolIndx] === getSymbolFromCryptoId(parseInt(register.symbol))) {
-              valuesArray[symbolIndx] = valuesArray[symbolIndx] ? valuesArray[symbolIndx] +  register.value : 0 + register.value
+            if (symbolsArray.indexOf(register.symbol) === -1) {
+              symbolsArray.push(register.symbol)
+            }
+          }
+          // console.log('Vuelta: ', register.symbol, 'symbolsArray: ', symbolsArray);
+        }
+        // console.log('symbolsArray: ', symbolsArray);
+        
+        for (let symbolIndx = 0; symbolIndx < symbolsArray.length; symbolIndx++) {
+          for (let register of registers) {
+            if (!register.isCrypto) {
+              if (symbolsArray[symbolIndx] === register.symbol) {
+                valuesArray[symbolIndx] = fixDecimals(valuesArray[symbolIndx] ? valuesArray[symbolIndx] +  register.value : 0 + register.value)
+              }
+            } else {
+              if (symbolsArray[symbolIndx] === getSymbolFromCryptoId(parseInt(register.symbol))) {
+                valuesArray[symbolIndx] = fixDecimals(valuesArray[symbolIndx] ? valuesArray[symbolIndx] +  register.value : 0 + register.value)
+              }
             }
           }
         }
-      }
-      for (let symbolIndx = 0; symbolIndx < symbolsArray.length; symbolIndx++) {
-        const crypto = getObjectFromCryptoSymbol(symbolsArray[symbolIndx])
-        if (crypto && crypto.symbol === symbolsArray[symbolIndx]) {
-          namesArray.push(crypto.name)
-          pricesArray.push(crypto.prices.price)
-        }
-        for (let currency of currenciesPrices.data) {
-          if (pricesArray[symbolIndx] === undefined) {
-            if (currency.symbol === symbolsArray[symbolIndx]) {
-              const currencyName = currenciesList.data.filter(el => el.symbol === symbolsArray[symbolIndx])[0].name
-              namesArray.push(currencyName)
-              pricesArray.push(currency.price)
+        for (let symbolIndx = 0; symbolIndx < symbolsArray.length; symbolIndx++) {
+          const crypto = getObjectFromCryptoSymbol(symbolsArray[symbolIndx])
+          if (crypto && crypto.symbol === symbolsArray[symbolIndx]) {
+            namesArray.push(crypto.name)
+            pricesArray.push(crypto.prices.price)
+          }
+          for (let currency of currenciesPrices.data) {
+            if (pricesArray[symbolIndx] === undefined) {
+              if (currency.symbol === symbolsArray[symbolIndx]) {
+                const currencyName = currenciesList.data.filter(el => el.symbol === symbolsArray[symbolIndx])[0].name
+                namesArray.push(currencyName)
+                pricesArray.push(currency.price)
+              }
             }
           }
         }
+        // console.log(symbolsArray, pricesArray);
+        // console.log(namesArray);
+        // console.log('Comparando:', valuesArray[0], pricesArray[0]);
+        // console.log('Comparando:', valuesArray[1], pricesArray[1]);
+        
+        // eslint-disable-next-line array-callback-return
+        symbolsArray.map((el, index) => {
+          const crypto = getObjectFromCryptoSymbol(el)
+          if (crypto) {
+            worthsArray.push(pricesArray[index] * valuesArray[index])
+          } else {
+            worthsArray.push(valuesArray[index] / pricesArray[index])
+          }
+        })
+        // console.log('User actives:', userActives);
+        // console.log('WorthsArray: ', worthsArray, '\n Total worth: ', totalWorth);
+        setTotalWorth(worthsArray.reduce((a, b) => a + b, 0))
+        // console.log(+totalWorth.toFixed(2));
+        setDetailedArray(symbolsArray.map((el, index) => {
+          return {
+            symbol: symbolsArray[index],
+            value: valuesArray[index],
+            valueUSD: worthsArray[index],
+            name: namesArray[index],
+            percentage: Math.round((worthsArray[index] / totalWorth) * 100),
+            price: pricesArray[index]
+          }
+        }))
+        console.log('usememo run');
+        
       }
-      // console.log(symbolsArray, pricesArray);
-      // console.log(namesArray);
-      // console.log('Comparando:', valuesArray[0], pricesArray[0]);
-      // console.log('Comparando:', valuesArray[1], pricesArray[1]);
-      
-      // eslint-disable-next-line array-callback-return
-      symbolsArray.map((el, index) => {
-        const crypto = getObjectFromCryptoSymbol(el)
-        if (crypto) {
-          worthsArray.push(pricesArray[index] * valuesArray[index])
-        } else {
-          worthsArray.push(valuesArray[index] / pricesArray[index])
-        }
-      })
-      // console.log('User actives:', userActives);
-      // console.log('WorthsArray: ', worthsArray);
-      const totalWorth = worthsArray.reduce((a, b) => a + b, 0)
-      // console.log(+totalWorth.toFixed(2));
-      const detailedArray = symbolsArray.map((el, index) => {
-        return {
-          symbol: symbolsArray[index],
-          value: valuesArray[index],
-          valueUSD: worthsArray[index],
-          name: namesArray[index],
-          percentage: Math.round((worthsArray[index] / totalWorth) * 100),
-          price: pricesArray[index]
-        }
-      })
-      // console.log('DetailedArray:' ,detailedArray);
-      
-      return {detailedArray, totalWorth}
     }
-  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userData, cryptoPrices.data, currenciesPrices.data])
 
-  const totalWorth = 0
+  console.log('WorthsArray: ', worthsArray, '\n Total worth: ', totalWorth);
 
-  return { totalWorth }
+  return { detailedArray, totalWorth }
 }
